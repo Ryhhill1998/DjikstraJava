6af3f90bb5dc6dd8b09d00ad6534a38abf65d304
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2022-12-15 15:42:19 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -8,18 +8,33 @@ public class Main {
         graph.addNode('D');
         graph.addNode('E');
 
-        graph.addConnection('A', 'B', 5);
-        graph.addConnection('A', 'C', 1);
-        graph.addConnection('B', 'A', 5);
+//        graph.addConnection('A', 'B', 5);
+//        graph.addConnection('A', 'C', 1);
+//        graph.addConnection('B', 'A', 5);
+//        graph.addConnection('B', 'C', 2);
+//        graph.addConnection('B', 'E', 1);
+//        graph.addConnection('C', 'A', 1);
+//        graph.addConnection('C', 'B', 2);
+//        graph.addConnection('C', 'D', 1);
+//        graph.addConnection('D', 'C', 1);
+//        graph.addConnection('D', 'E', 3);
+//        graph.addConnection('E', 'B', 1);
+//        graph.addConnection('E', 'D', 3);
+
+        graph.addConnection('A', 'B', 1);
+        graph.addConnection('A', 'D', 3);
+        graph.addConnection('B', 'A', 1);
         graph.addConnection('B', 'C', 2);
-        graph.addConnection('B', 'E', 1);
-        graph.addConnection('C', 'A', 1);
+        graph.addConnection('B', 'D', 1);
         graph.addConnection('C', 'B', 2);
-        graph.addConnection('C', 'D', 1);
-        graph.addConnection('D', 'C', 1);
+        graph.addConnection('C', 'D', 2);
+        graph.addConnection('C', 'E', 1);
+        graph.addConnection('D', 'A', 3);
+        graph.addConnection('D', 'B', 1);
+        graph.addConnection('D', 'C', 2);
         graph.addConnection('D', 'E', 3);
-        graph.addConnection('E', 'B', 1);
         graph.addConnection('E', 'D', 3);
+        graph.addConnection('E', 'C', 1);
 
         graph.printGraph();
         System.out.println();
